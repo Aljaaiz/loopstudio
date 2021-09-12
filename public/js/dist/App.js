72385dac5f7003b0998e8b68526c216888250d60
Aljaaiz <aljaaiz1@gmail.com>
2021-09-12 21:52:36 +0400

    about us fixed

--- public/js/dist/App.js
+++ public/js/dist/App.js
@@ -1,10 +1,12 @@
-let hamburger = document.getElementById("hamburger");
-let mobile_nav = document.querySelector(".mobile-nav");
-let close = document.getElementById("close-hamburger");
+let hamburger = document.getElementById('hamburger');
+let mobile_nav = document.querySelector('.mobile-nav');
+let close = document.getElementById('close-hamburger');
 
-hamburger.addEventListener("click", function () {
-  mobile_nav.classList.toggle("active");
+hamburger.addEventListener('click', function() {
+	mobile_nav.classList.toggle('active');
 });
-close.addEventListener("click", function () {
-  mobile_nav.classList.remove("active");
+close.addEventListener('click', function() {
+	mobile_nav.classList.remove('active');
 });
+let screen = window.devicePixelRatio;
+console.log(screen);
